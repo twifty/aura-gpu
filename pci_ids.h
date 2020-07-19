9173f5664539f6206db0cd537197df17b38de3b3
Kevin Adler <adler187@gmail.com>
2020-07-19 11:21:46 -0500

    Add support for ROG STRIX-RX480-8G-GAMING
    Signed-off-by: Kevin Adler <adler187@gmail.com>

--- pci_ids.h
+++ pci_ids.h
@@ -10,6 +10,7 @@
     NVIDIA devices already have the adapter loaded.
  */
 static const struct pci_device_id pciidlist[] = {
+    {0x1002, 0x67df, 0x1043, 0x04fd, 0, 0, CHIP_POLARIS10},     // RX480 (Strix)
     {0x1002, 0x67df, 0x1043, 0x0517, 0, 0, CHIP_POLARIS10},     // RX580 (Strix)
     {0x1002, 0x687F, 0x1043, 0x0555, 0, 0, CHIP_VEGA10},        // Vega 56 (Strix)
     // {0x1002, 0x731f, 0x1043, 0x04e2, 0, 0, CHIP_NAVI10},     // RX5700XT (Strix)
